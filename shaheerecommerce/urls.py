b0e7f9c57be8567883fbe71e53683c40e0e2acf1
shaheermudassar <124002206+shaheermudassar@users.noreply.github.com>
2023-05-12 06:25:49 +0500

    Update urls.py

--- shaheerecommerce/urls.py
+++ shaheerecommerce/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path
 
 
 urlpatterns = [
@@ -26,8 +26,8 @@ urlpatterns = [
     path('',include("core.urls")),
     path('user/',include("userauths.urls")),
     path('ckeditor/', include("ckeditor_uploader.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static2/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static2/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
