259b444705229859e3b21eacf9b15f9e80d12b01
shaheermudassar <shaheer.mudassar13@gmail.com>
2023-05-14 14:51:22 +0500

    Update settings.py

--- shaheerecommerce/settings.py
+++ shaheerecommerce/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bwzjken21^3u^2c+#locm%ok#gop^aqu897qdxy%mu)d=rt&mz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1","shaheermart.herokuapp.com"]
 
