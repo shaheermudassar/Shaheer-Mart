fa59138a01f679d06ae0bb9c57b681d34f4b74af
shaheermudassar <124002206+shaheermudassar@users.noreply.github.com>
2023-06-04 10:16:35 +0500

    Update urls.py

--- shaheerecommerce/urls.py
+++ shaheerecommerce/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.urls import re_path
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +29,6 @@ urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     re_path(r'^static2/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
-
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
